2b29e63a0aacc84ed0a914f6abd6075b7fbc05f7
SergioDeLaHoz16 <sddelahoz@unicesar.edu.co>
2025-01-19 15:03:23 -0500

    feat: add image constants for app and social logos

---
+++ lib/utils/constants/image_string.dart
@@ -0,0 +1,12 @@
+class CImages {
+  // App Logo
+  static const String darkAppLogo =
+      'D:/Flutter_Trabajos/retail_sync/assets/logos/logo.png';
+  static const String lightAppLogo =
+      'D:/Flutter_Trabajos/retail_sync/assets/logos/logo.png';
+
+  // Social Logos
+  static const String google =
+      'D:/Flutter_Trabajos/retail_sync/assets/logos/google-icon.png';
+  static const String appIcon2 = '';
+}
